d6b68b80af3edd0966191e05b8166abe62cb3fb7
kylin1875 <zhiweizheng@Zhiweis-MacBook-Pro.local>
2016-07-21 18:25:28 -0700

    t

--- server.js
+++ server.js
@@ -1,65 +1,60 @@
 // Load required modules
-var http = require("http");      // http server core module
-var express = require("express");// web framework external module
-var io = require("socket.io"); // web socket external module
-var easyrtc = require("easyrtc");  // EasyRTC external module
+var http    = require("http");              // http server core module
+var express = require("express");           // web framework external module
+var serveStatic = require('serve-static');  // serve static files
+var socketIo = require("socket.io");        // web socket external module
+var easyrtc = require("easyrtc");               // EasyRTC external module
+var port = process.env.PORT || 8080;
+// Set process name
+process.title = "easyrtc";
 
-// Setup and configure Express http server. Expect 
-// a subfolder called "static" to be the web root.
-var httpApp = express();
-httpApp.use(express.static(__dirname + "/static/"));
+// Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
+var app = express();
+app.use(serveStatic('static', {'index': ['index.html']}));
 
 // Start Express http server on port 8080
-var webServer = http.createServer(httpApp).listen(process.env.PORT || 5050);
+var port = process.env.PORT || 8080;
+var webServer = http.createServer(app).listen(port);
 
 // Start Socket.io so it attaches itself to Express server
-var socketServer = io.listen(webServer, {"log level":1});
+var socketServer = socketIo.listen(webServer, {"log level":1});
 
 easyrtc.setOption("logLevel", "debug");
 
-// Overriding the default easyrtcAuth listener, only so we can 
-// directly access its callback
-easyrtc.events.on("easyrtcAuth", function(socket, easyrtcid, msg, 
-									socketCallback, callback) 
-{
-		easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, 
-						msg, socketCallback, function(err, connectionObj){
-				if (err || !msg.msgData || !msg.msgData.credential 
-								|| !connectionObj) {
-						callback(err, connectionObj);
-						return;
-				}
+// Overriding the default easyrtcAuth listener, only so we can directly access its callback
+easyrtc.events.on("easyrtcAuth", function(socket, easyrtcid, msg, socketCallback, callback) {
+	easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function(err, connectionObj){
+		if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
+			callback(err, connectionObj);
+			return;
+		}
 
-				connectionObj.setField("credential", msg.msgData.credential, 
-						{"isShared":false});
+		connectionObj.setField("credential", msg.msgData.credential, {"isShared":false});
 
-				console.log("["+easyrtcid+"] Credential saved!", 
-						connectionObj.getFieldValueSync("credential"));
+		console.log("["+easyrtcid+"] Credential saved!", connectionObj.getFieldValueSync("credential"));
 
-				callback(err, connectionObj);
-		});
+		callback(err, connectionObj);
+	});
 });
 
 // To test, lets print the credential to the console for every room join!
-easyrtc.events.on("roomJoin", function(connectionObj, roomName, 
-									roomParameter, callback) 
-{
-		console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", 
-								connectionObj.getFieldValueSync("credential"));
-		easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, 
-								roomParameter, callback);
+easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, callback) {
+	console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
+	easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
 });
 
 // Start EasyRTC server
-var rtc = easyrtc.listen(httpApp, socketServer, null, 
-								function(err, rtcRef) {
-		console.log("Initiated");
+var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
+	console.log("Initiated");
 
-		rtcRef.events.on("roomCreate", function(appObj, 
-								creatorConnectionObj, roomName, roomOptions, callback) {
-				console.log("roomCreate fired! Trying to create: " + roomName);
+	rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
+		console.log("roomCreate fired! Trying to create: " + roomName);
 
-				appObj.events.defaultListeners.roomCreate(appObj, 
-								creatorConnectionObj, roomName, roomOptions, callback);
-		});
+		appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
+	});
+});
+
+//listen on port 8080
+webServer.listen(port, function () {
+	console.log('listening on'+port);
 });
